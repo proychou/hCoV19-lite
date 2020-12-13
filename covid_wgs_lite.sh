03adebec7a43d668eed5245572e09f6ba4f343e6
Pavitra <pavitra@Pavitras-MacBook-Pro.local>
2020-12-12 23:03:51 -0800

    Update covid_wgs_lite.sh
    switch to bbmap

--- covid_wgs_lite.sh
+++ covid_wgs_lite.sh
@@ -216,11 +216,11 @@ fi
 printf "\n\nMapping reads to reference ... \n\n\n"
 mkdir -p ./mapped_reads
 mappedtoref_bam='./mapped_reads/'$sampname'.bam'
-bowtie2 -x ./refs/$ref_bowtie -U $processed_fastq -p ${SLURM_CPUS_PER_TASK} | samtools view -bS -F 4 - > $mappedtoref_bam
+# bowtie2 -x ./refs/$ref_bowtie -U $processed_fastq -p ${SLURM_CPUS_PER_TASK} | samtools view -bS -F 4 - > $mappedtoref_bam
+bbmap.sh in=$processed_fastq outm=$mappedtoref_bam ref=$ref_fasta maxindel=1000 
 samtools sort -@ ${SLURM_CPUS_PER_TASK} -o './mapped_reads/'$sampname'.sorted.bam' $mappedtoref_bam 
 rm $mappedtoref_bam 
 mv './mapped_reads/'$sampname'.sorted.bam' $mappedtoref_bam 
-
 
 
 fi
