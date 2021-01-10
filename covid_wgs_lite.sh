3795b00dc0042798c8092375e90eb6be1b236afc
Pavitra <pavitra@Pavitras-MacBook-Pro.local>
2021-01-10 15:51:17 -0800

    Update covid_wgs_lite.sh
    filter bams to remove mapq<2

--- covid_wgs_lite.sh
+++ covid_wgs_lite.sh
@@ -134,7 +134,7 @@ fi
 printf "\n\nMapping reads to reference ... \n\n\n"
 mkdir -p ./mapped_reads
 mappedtoref_bam='./mapped_reads/'$sampname'.bam'
-bowtie2 -x ./refs/$ref_bowtie -1 $processed_fastq1 -2 $processed_fastq2 -p ${SLURM_CPUS_PER_TASK} | samtools view -bS -F 4 - > $mappedtoref_bam
+bowtie2 -x ./refs/$ref_bowtie -1 $processed_fastq1 -2 $processed_fastq2 -p ${SLURM_CPUS_PER_TASK} | samtools view -bS -q 2 -F 4 - > $mappedtoref_bam
 # bbmap.sh in1=$processed_fastq1 in2=$processed_fastq2 outm=$mappedtoref_bam ref=$ref_fasta maxindel=1000 sam=1.3
 samtools sort -@ ${SLURM_CPUS_PER_TASK} -o './mapped_reads/'$sampname'.sorted.bam' $mappedtoref_bam 
 rm $mappedtoref_bam 
@@ -217,7 +217,7 @@ fi
 printf "\n\nMapping reads to reference ... \n\n\n"
 mkdir -p ./mapped_reads
 mappedtoref_bam='./mapped_reads/'$sampname'.bam'
-bowtie2 -x ./refs/$ref_bowtie -U $processed_fastq -p ${SLURM_CPUS_PER_TASK} | samtools view -bS -F 4 - > $mappedtoref_bam
+bowtie2 -x ./refs/$ref_bowtie -U $processed_fastq -p ${SLURM_CPUS_PER_TASK} | samtools view -bS -q 2 -F 4 - > $mappedtoref_bam
 # bbmap.sh in=$processed_fastq outm=$mappedtoref_bam ref=$ref_fasta maxindel=1000 sam=1.3
 samtools sort -@ ${SLURM_CPUS_PER_TASK} -o './mapped_reads/'$sampname'.sorted.bam' $mappedtoref_bam 
 rm $mappedtoref_bam 
